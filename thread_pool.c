335e88a5379b8717c8b6c317532491cf8e5b1b75
zhenghuashan <zheng.huashan@163.com>
2015-12-27 10:48:42 +0800

    Update thread_pool.c

--- thread_pool.c
+++ thread_pool.c
@@ -1,1 +1,12 @@
 #include "thread_pool.h"
+int thread_pool_init(thread_pool *self, int max_thread_num){
+  
+  self = (thread_pool*)malloc(sizeof(thread_pool));
+  self->cur_task_num = 0;
+  self->max_thread_num = max_thread_num;
+  pthread_mutex_init(&self->task_list_mutext);
+  
+  
+  
+  
+}
