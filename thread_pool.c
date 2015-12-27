2e11a67b0cc266bd04553026bc5fdee089609fa6
zhenghuashan <zheng.huashan@163.com>
2015-12-27 11:10:27 +0800

    Update thread_pool.c

--- thread_pool.c
+++ thread_pool.c
@@ -18,6 +18,29 @@ int thread_pool_init(thread_pool *self, int max_thread_num){
   for (i = 0; i < self->max_thread_num; i++){
     pthread_create(&self->thread_id_array[i], NULL, thread_pool_thread_routine, NULL);
   }
+  self->shutdown = 0;
   
   return 0;
+}
+
+
+void thread_pool_destory(thread_pool *self){
+  int i;
+  
+  if (self->shutdown){
+    return;
+  }
+  self->shutdown = 1;
+  pthread_cond_broadcast(&self->task_read_cond);
+  /*等待所有线程退出*/
+  for (i = 0; i < self->max_thread_num; i++){
+    pthread_join(&self->thread_id_array[i]);
+  }
+  free(self->thread_id_array);
+  pthread_mutex_destory(&self->task_list_mutext);
+  pthread_cond_destroy(&self->task_read_cond);
+  free(self);
+  self = NULL;
+  
+  
 }
