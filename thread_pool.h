2385ac73a4d6e5126ad941d97d637a5b077fd9d4
zhenghuashan <zheng.huashan@163.com>
2016-01-08 16:02:09 +0800

    Update thread_pool.h

--- thread_pool.h
+++ thread_pool.h
@@ -1,47 +1,46 @@
 #ifndef THREAD_POOL_H
 #define THREAD_POOL_H
-#include <pthread.h>
 
 
-
+typedef struct thread_pool_s thread_pool_t;
 typedef void * (*thread_func_t)(void *);
 
-typedef struct task_node_s{
-	thread_func_t 			thread_func;
-    void 			    	*arg;
-    struct task_node_s 	    *next;
-}task_node_t;
-
-typedef struct task_deque_s{
-    int 			    	deque_len;
-	task_node_t             *head;
-	pthread_mutex_t 		mutex;	
-}task_deque_t;
-
-typedef struct thread_pool_s{
-    int 						max_thread_num;
-    int 						cur_task_num;
-	volatile int	            working_threads_num; /*工作任务数*/
-	volatile int	            alive_threads_num; /*工作任务数*/
-    int 						shutdown;
-	volatile int						keep_alive;
-    task_deque_t 				*task_deque;
-    pthread_mutex_t 		threads_num_mutex;
-	pthread_cond_t  		threads_all_idle_cond;
-	
-	/*用于线程池调度*/
-	pthread_mutex_t         task_mutex;
-	int v;
-    pthread_cond_t  		task_cond;
-
-    pthread_t 				*thread_id_array;
- 
-}thread_pool_t;
-
-
+/*
+创建线程池
+参数:
+int max_thread_num 线程个数
+返回值：
+成功返回线程池对象，失败返回NULL
+*/
 thread_pool_t* thread_pool_create(int max_thread_num);
-void thread_pool_add_task(thread_pool_t *self, thread_func_t thread_func, void *arg);
-int  thread_pool_destroy(thread_pool_t *self);
+
+/*
+往线程池放入一个任务
+参数:
+thread_pool_t *self 线程池对象
+thread_func_t thread_func 执行任务函数
+void *arg					参数
+返回值：
+0代表成功，-1代表失败
+失败原因是当前任务数大于最大任务数
+*/
+int  thread_pool_add_task(thread_pool_t *self, thread_func_t thread_func, void *arg);
+
+/*
+等待所有任务执行完
+参数:
+thread_pool_t *self 线程池对象
+*/
 void thread_pool_wait(thread_pool_t *self);
+
+/*
+销毁线程池
+参数:
+thread_pool_t *self 线程池对象
+返回值：
+0代表成功，-1代表失败
+*/
+int thread_pool_destroy(thread_pool_t *self);
+
 
 #endif // THREAD_POOL_H
